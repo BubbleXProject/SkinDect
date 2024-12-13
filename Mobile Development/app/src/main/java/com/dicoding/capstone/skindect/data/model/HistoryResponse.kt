114066188ad443a2ce56161705fc74c6d58192a1
Rifky Fahriza Sinaga <116672301+BubbleXProject@users.noreply.github.com>
2024-12-13 20:50:54 +0700

    Delete Mobile Development directory

--- Mobile Development/app/src/main/java/com/dicoding/capstone/skindect/data/model/HistoryResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.dicoding.capstone.skindect.data.model
-
-data class HistoryResponse(
-    var id: String = "",
-    val image_url: String = "",
-    val diagnosis: String = "",
-    val recommendation: String = "",
-    val timestamp: Long = 0L
-)
